249d98ed3041571a6b143f9448214adcf38bc856
David Heyman <dave@axismaps.com>
2020-11-21 23:44:32 +0000

    Working name search

---
+++ routes/nameSearch.js
@@ -0,0 +1,27 @@
+const { Layer, Sequelize } = require('../models');
+
+module.exports = (router) => {
+  router.get('/search', (req, res) => {
+    const { text, year } = req.query;
+    if (!text || !year) return res.sendStatus(500);
+    return Layer.findAll({
+      attributes: ['id', 'title'],
+      include: {
+        association: 'Features',
+        attributes: ['id', 'name'],
+        where: {
+          firstyear: {
+            [Sequelize.Op.lte]: parseInt(year, 10),
+          },
+          lastyear: {
+            [Sequelize.Op.gte]: parseInt(year, 10),
+          },
+          name: {
+            [Sequelize.Op.iLike]: `%${text}%`,
+          },
+        },
+        required: true,
+      },
+    }).then((layers) => res.send(layers));
+  });
+};
