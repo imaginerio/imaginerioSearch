a6700a579dcaed4cee5892e5e88d01c08b28dbad
David Heyman <davidheyman@ZUBEIA.cable.virginm.net>
2020-12-01 09:31:17 +0000

    Starting feature import

--- seeders/20201121225338-load-features.js
+++ seeders/20201121225338-load-features.js
@@ -1,36 +1,59 @@
+/* eslint-disable no-console */
 const axios = require('axios');
 const { nanoid } = require('nanoid');
+const { range } = require('lodash');
 const { Feature, Layer } = require('../models');
+
+const STEP = 200;
 
 module.exports = {
   up: async () => {
+    const stepLoader = (layer, i, count) =>
+      axios
+        .get(
+          `https://arcgis.rice.edu/arcgis/rest/services/imagineRio_Data/FeatureServer/${layer.remoteId}/query?where=name IS NOT NULL&outFields=objectid,name,firstyear,lastyear&f=geojson&resultRecordCount=${STEP}&resultOffset=${i}`
+        )
+        .then(({ data: { features } }) => {
+          console.log(`${i} / ${count}`);
+          const featureLoader = features.map(feature =>
+            Feature.create({
+              ...feature.properties,
+              id: `i${nanoid(8)}`,
+              LayerId: layer.id,
+              geom: feature.geometry,
+            })
+          );
+          return Promise.all(featureLoader);
+        });
+
+    const layerLoader = async l => {
+      console.log(`Loading ${l.name}`);
+      const layer = await Layer.create();
+      console.log(layer);
+      const {
+        data: { count },
+      } = await axios.get(
+        `https://arcgis.rice.edu/arcgis/rest/services/imagineRio_Data/FeatureServer/${l.id}/query?where=objectid IS NOT NULL&f=json&returnCountOnly=true`
+      );
+
+      console.log(count);
+
+      range(0, count, STEP).reduce(async (previousPromise, next) => {
+        await previousPromise;
+        return stepLoader(layer, next, count);
+      }, Promise.resolve());
+    };
+
     let {
       data: { layers },
     } = await axios.get(
       'https://arcgis.rice.edu/arcgis/rest/services/imagineRio_Data/FeatureServer/layers?f=json'
     );
     layers = layers.filter(l => !l.name.match(/^ir_rio/));
-    return Promise.all(
-      layers.map(l =>
-        Layer.create({ title: l.name, remoteId: l.id }).then(layer =>
-          axios
-            .get(
-              `https://arcgis.rice.edu/arcgis/rest/services/imagineRio_Data/FeatureServer/${layer.remoteId}/query?where=name IS NOT NULL&outFields=objectid,name,firstyear,lastyear&f=geojson`
-            )
-            .then(({ data: { features } }) => {
-              const featureLoader = features.map(feature =>
-                Feature.create({
-                  ...feature.properties,
-                  id: `i${nanoid(8)}`,
-                  LayerId: layer.id,
-                  geom: feature.geometry,
-                })
-              );
-              return Promise.all(featureLoader);
-            })
-        )
-      )
-    );
+    layers.reduce(async (previousPromise, next) => {
+      await previousPromise;
+      return layerLoader(next);
+    }, Promise.resolve());
   },
 
   down: async queryInterface =>
