0e889716fa21c979aeb0fcf3d110814008330904
David Heyman <dave@axismaps.com>
2020-12-01 10:56:30 +0000

    Changing config process

---
+++ config/config.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: 'postgres',
+    password: 'postgres',
+    database: 'postgres',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  test: {
+    username: 'postgres',
+    password: 'postgres',
+    database: 'postgres_test',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  production: {
+    url: process.env.DB_URL,
+    dialect: 'postgres',
+  },
+};
