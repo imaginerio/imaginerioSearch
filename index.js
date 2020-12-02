33dca58089d39ffb074d02bf08efb4b43a97e5f6
David Heyman <dave@axismaps.com>
2020-12-02 10:22:30 +0000

    Probe method with rate limiter

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 /* eslint-disable no-console */
 const express = require('express');
 const bodyparser = require('body-parser');
+const rateLimit = require('express-rate-limit');
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.set('trust proxy', 1);
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+app.use(limiter);
+
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 
-require('./routes/nameSearch')(app);
+require('./routes/search')(app);
 require('./routes/layers')(app);
+require('./routes/probe')(app);
 
 app.listen(port, () => console.log(`Server started on ${port}`));
---
+++ routes/probe.js
@@ -0,0 +1,30 @@
+const { Layer, Sequelize } = require('../models');
+
+module.exports = router => {
+  router.get('/probe/:location', (req, res) => {
+    const { year } = req.query;
+    const location = req.params.location.split(',');
+    if (location.length !== 2 || location.length !== 4 || !year) return res.sendStatus(500);
+    let geom = Sequelize.fn('ST_SetSRID', Sequelize.fn('ST_MakePoint', ...location), 4326);
+    if (location.length === 4) geom = Sequelize.fn('ST_MakeEnvelope', ...location, 4326);
+    return Layer.findAll({
+      attributes: ['id', 'title'],
+      include: {
+        association: 'Features',
+        attributes: ['id', 'name'],
+        where: {
+          firstyear: {
+            [Sequelize.Op.lte]: parseInt(year, 10),
+          },
+          lastyear: {
+            [Sequelize.Op.gte]: parseInt(year, 10),
+          },
+          geom: {
+            [Sequelize.Op.overlap]: geom,
+          },
+        },
+        limit: 5,
+      },
+    }).then(layers => res.send(layers.filter(l => l.Features.length)));
+  });
+};
