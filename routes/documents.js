99aa44892e6635b8d71344020472c731b561c765
David Heyman <dave@axismaps.com>
2020-12-07 19:54:05 +0000

    Don't send a year to get all documents

--- routes/documents.js
+++ routes/documents.js
@@ -3,20 +3,24 @@ const { Visual, Sequelize } = require('../models');
 module.exports = router => {
   router.get('/documents', (req, res) => {
     const { year } = req.query;
-    if (!year) return res.sendStatus(500);
+    let where = {};
+    if (year) {
+      where = {
+        firstyear: {
+          [Sequelize.Op.lte]: parseInt(year, 10),
+        },
+        lastyear: {
+          [Sequelize.Op.gte]: parseInt(year, 10),
+        },
+      };
+    }
+
     return Visual.findAll({
       attributes: ['id', 'title'],
       include: {
         association: 'Documents',
         attributes: ['ssid', 'title', 'latitude', 'longitude', 'firstyear', 'lastyear'],
-        where: {
-          firstyear: {
-            [Sequelize.Op.lte]: parseInt(year, 10),
-          },
-          lastyear: {
-            [Sequelize.Op.gte]: parseInt(year, 10),
-          },
-        },
+        where,
       },
     }).then(layers => res.send(layers.filter(l => l.Documents.length)));
   });
