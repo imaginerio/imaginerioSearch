f82d8a2f5aabc15de5cad4f3aafb2d9f65a45656
David Heyman <dave@axismaps.com>
2020-12-11 11:58:46 +0000

    Send name with GeoJSON

--- routes/feature.js
+++ routes/feature.js
@@ -4,10 +4,14 @@ module.exports = router => {
   router.get('/feature/:id', (req, res) => {
     const { id } = req.params;
     if (!id) return res.sendStatus(500);
-    return Feature.findByPk(id, { attributes: ['geom'] }).then(geometry =>
+    return Feature.findByPk(id, { attributes: ['id', 'name', 'geom'] }).then(geometry =>
       res.send({
         type: 'Feature',
         geometry: geometry.geom,
+        properties: {
+          id: geometry.id,
+          name: geometry.name,
+        },
       })
     );
   });
