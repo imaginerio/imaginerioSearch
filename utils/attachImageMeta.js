bae3c8a3037471c9816d8ce491054bfaf67e82eb
David Heyman <dave@axismaps.com>
2021-08-16 11:39:33 +0000

    Updated handling for width / height

--- utils/attachImageMeta.js
+++ utils/attachImageMeta.js
@@ -1,3 +1,5 @@
+const { some } = require('lodash');
+
 exports.processImageMeta = meta => {
   let value = meta.value.length > 1 ? meta.value : meta.value[0];
   if (value && !Array.isArray(value) && value.match(/^-?\d+\.?\d*$/)) {
@@ -14,5 +16,26 @@ exports.attachImageMeta = imageMeta => {
     const [label] = meta.label.split(' ');
     document[label.toLowerCase()] = link ? { value, link } : value;
   });
+
+  if (
+    some(imageMeta, ({ label }) => label === 'Width') &&
+    some(imageMeta, ({ label }) => label === 'Height')
+  ) {
+    document.width = parseInt(imageMeta.find(({ label }) => label === 'Width').value[0], 10);
+    document.height = parseInt(imageMeta.find(({ label }) => label === 'Height').value[0], 10);
+  } else if (
+    some(imageMeta, ({ label }) => label.match(/Width \(.*\)/gi)) &&
+    some(imageMeta, ({ label }) => label.match(/Height \(.*\)/gi))
+  ) {
+    document.width = parseInt(
+      imageMeta.find(({ label }) => label.match(/Width \(.*\)/gi)).value[0],
+      10
+    );
+    document.height = parseInt(
+      imageMeta.find(({ label }) => label.match(/Height \(.*\)/gi)).value[0],
+      10
+    );
+  }
+
   return document;
 };
