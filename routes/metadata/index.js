7424a01c3b1494413cf03ed8a9d7cb9fd50d7503
David Heyman <dave@axismaps.com>
2021-11-01 15:10:00 +0000

    Testing language in metadata route

--- routes/metadata/index.js
+++ routes/metadata/index.js
@@ -2,15 +2,17 @@ const { Document, Sequelize } = require('../../models');
 const { processImageMeta } = require('../../utils/attachImageMeta');
 
 module.exports = router => {
-  router.get('/metadata/:id', (req, res) =>
-    Document.findOne({
+  router.get('/metadata/:id', (req, res) => {
+    const { lang } = req.query;
+    return Document.findOne({
       where: {
         [Sequelize.Op.or]: [{ ssid: req.params.id }, { id: req.params.id }],
       },
       include: {
         association: 'ImageMeta',
         attributes: ['label', 'value', 'link'],
+        where: [lang, 'none'],
       },
-    }).then(document => res.send(document.ImageMeta.map(m => processImageMeta(m))))
-  );
+    }).then(document => res.send(document.ImageMeta.map(m => processImageMeta(m))));
+  });
 };
